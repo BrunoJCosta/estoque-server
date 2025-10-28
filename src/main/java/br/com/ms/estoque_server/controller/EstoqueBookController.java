153b7cb4bf80e9b66763f33d95018ef97777bf9d
BrunoJCosta <brunojereissati00@gmail.com>
2025-10-28 19:56:10 -0300

    feat(estoque): organized the controller

--- src/main/java/br/com/ms/estoque_server/controller/EstoqueBookController.java
+++ src/main/java/br/com/ms/estoque_server/controller/EstoqueBookController.java
@@ -17,25 +17,18 @@ import java.util.List;
 @RestController
 @RequestMapping("/estoque")
 @RequiredArgsConstructor
-public class BookController {
+public class EstoqueBookController {
 
     private final BookService bookService;
 
-    @GetMapping("/id/{id}")
-    public ResponseEntity<TemplateDTO> findById(@PathVariable Long id) throws BookNaoEncontrado {
-        TemplateDTO templateDTO = bookService.findById(id)
-                .orElseThrow(BookNaoEncontrado::new);
-        return ResponseEntity.ok(templateDTO);
-    }
-
-    @GetMapping("/referencia/{referencia}")
+    @GetMapping("/book/{referencia}")
     public ResponseEntity<TemplateDTO> findByReferencia(@PathVariable Long referencia) throws BookNaoEncontrado {
         TemplateDTO templateDTO = bookService.findByReferencia(referencia)
                 .orElseThrow(BookNaoEncontrado::new);
         return ResponseEntity.ok(templateDTO);
     }
 
-    @GetMapping()
+    @GetMapping("/book/listagem")
     public ResponseEntity<List<TemplateDTO>> findAll() {
         List<TemplateDTO> templateDTO = bookService.findAll();
         return ResponseEntity.ok(templateDTO);

1c5f80a10b8f6c755b781627ee9d78b0c6d82efc
BrunoJCosta <brunojereissati00@gmail.com>
2025-10-29 20:10:10 -0300

    feat(estoque): fix the redis

--- src/main/java/br/com/ms/estoque_server/configuration/redis/RedisConfiguration.java
+++ src/main/java/br/com/ms/estoque_server/configuration/redis/RedisConfiguration.java
@@ -1,15 +1,16 @@
 package br.com.ms.estoque_server.configuration.redis;
 
 import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.stereotype.Component;
-import redis.clients.jedis.Jedis;
 
-@Component
+@Configuration
+@EnableCaching
 public class RedisConfiguration {
 
     @Bean
@@ -20,14 +21,7 @@ public class RedisConfiguration {
         config.setPassword("bruno_application");
         config.setDatabase(2);
 
-        JedisConnectionFactory factory = new JedisConnectionFactory(config);
-        // Teste a conexão
-        try (Jedis jedis = (Jedis) factory.getConnection().getNativeConnection()) {
-            System.out.println("Conexão com Redis bem-sucedida: " + jedis.ping());
-        } catch (Exception e) {
-            System.err.println("Erro ao conectar ao Redis: " + e.getMessage());
-        }
-        return factory;
+        return new JedisConnectionFactory(config);
     }
 
     @Bean

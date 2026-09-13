e252c051a70ad9c3bf81039defabf98c95121edf
BrunoJCosta <brunojereissati00@gmail.com>
2026-09-13 12:04:15 -0300

    feat(Redis): configuration to up the docker

--- src/main/java/br/com/ms/estoque_server/configuration/redis/RedisConfiguration.java
+++ src/main/java/br/com/ms/estoque_server/configuration/redis/RedisConfiguration.java
@@ -1,9 +1,11 @@
 package br.com.ms.estoque_server.configuration.redis;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
@@ -11,13 +13,20 @@ import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 
 @Configuration
 @EnableCaching
+@Profile("!test")
 public class RedisConfiguration {
+
+    @Value("${REDIS}")
+    private String hostName;
+
+    @Value("${REDIS_PORT}")
+    private Integer port;
 
     @Bean
     public JedisConnectionFactory redisConnectionFactory() {
         RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
-        config.setHostName("localhost");
-        config.setPort(6379);
+        config.setHostName(hostName);
+        config.setPort(port);
         config.setPassword("bruno_application");
         config.setDatabase(2);
 
